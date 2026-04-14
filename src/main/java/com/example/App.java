29b0a04a4b16679467f811313f2e780f1d48f087
Numan Khan <133324771+numankhanssk@users.noreply.github.com>
2026-04-14 11:18:01 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,19 @@
 package com.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class App {
 
@@ -24,44 +33,39 @@ public class App {
 
             WebDriver driver = new ChromeDriver(options);
 
-            // Open SauceDemo
             driver.get("https://automationexercise.com/");
-
-            // Maximize (works in non-headless too; safe to keep)
             driver.manage().window().maximize();
 
-            // Create explicit wait
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
             // -------------------------------
-            // Step 1: Click on Products link
+            // Step 1: Click Products
             // -------------------------------
             WebElement productsLink = driver.findElement(By.cssSelector("a[href='/products']"));
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", productsLink);
             ((JavascriptExecutor) driver).executeScript("arguments[0].click();", productsLink);
-            Thread.sleep(2000); // 2 sec delay to see it
-    
+            Thread.sleep(2000);
+
             // -------------------------------
-            // Step 2: Click Add to Cart button
+            // Step 2: Add to cart
             // -------------------------------
-            WebElement addToCart = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.add-to-cart")));
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addToCart);
+            WebElement addToCart = wait.until(
+                    ExpectedConditions.elementToBeClickable(By.cssSelector(".add-to-cart"))
+            );
             ((JavascriptExecutor) driver).executeScript("arguments[0].click();", addToCart);
-            Thread.sleep(2000); // 2 sec delay to see the modal
-    
+            Thread.sleep(2000);
+
             // -------------------------------
-            // Step 3: Close modal popup
+            // Step 3: Close modal
             // -------------------------------
-            WebElement closeBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.close-modal")));
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", closeBtn);
+            WebElement closeBtn = wait.until(
+                    ExpectedConditions.elementToBeClickable(By.cssSelector(".close-modal"))
+            );
             ((JavascriptExecutor) driver).executeScript("arguments[0].click();", closeBtn);
-            Thread.sleep(2000); // 2 sec delay to see it close
+            Thread.sleep(2000);
 
+            System.out.println("TITLE: " + driver.getTitle());
 
-            // Print after login
-            System.out.println("TITLE AFTER LOGIN: " + driver.getTitle());
-
-            // Close browser
             driver.quit();
 
         } catch (Exception e) {
